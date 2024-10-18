7231ded41e74e4ce31bdb725125b4ca131b4c789
Deubli <135395146+Deubli@users.noreply.github.com>
2024-10-18 07:59:59 -0400

    ACTUALLY finished

--- Assets/isotopeScript.cs
+++ Assets/isotopeScript.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class isotopeScript : MonoBehaviour
 {
     public GameObject[] isotopes;
@@ -10,15 +10,18 @@ public class isotopeScript : MonoBehaviour
     {
         Destroy(GameObject.Find("Atom(Clone)"));
         Instantiate(isotopes[0], new Vector3(0,0,0), Quaternion.identity);
+        GameObject.Find("NeutronAmount").GetComponent<TMP_Text>().text = ("Neutrons: " + 16);
     }
     public void SecondMost()
     {
         Destroy(GameObject.Find("Atom(Clone)"));
         Instantiate(isotopes[1], new Vector3(0,0,0), Quaternion.Euler(0, 0, -45));
+        GameObject.Find("NeutronAmount").GetComponent<TMP_Text>().text = ("Neutrons: " + 18);        
     }
     public void ThirdMost()
     {
         Destroy(GameObject.Find("Atom(Clone)"));
         Instantiate(isotopes[2], new Vector3(0,0,0), Quaternion.Euler(0, 0, 45));
+        GameObject.Find("NeutronAmount").GetComponent<TMP_Text>().text = ("Neutrons: " + 17);
     }
 }
